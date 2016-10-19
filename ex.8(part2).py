e60a8b3d7e22db2d593a763b4de0e6ec56f541c7
DimitrisParris <parrisdim@hotmail.com>
2016-10-19 22:26:46 +0300

    Delete ex.8(part2).py

--- ex.8(part2).py
+++
@@ -1,9 +0,0 @@
-num = input("Give sequence of numbers:")
-sum = 0
-for i in range(len(num)):
-    if i%2 == 0 and i<(len(num)-1):
-        a = int(num[i])*int(num[i+1])
-        sum+=a
-    if len(num)%2 !=0 and i==(len(num)-1):
-       sum += int(num[-1])
-print("This is the sum of the sequence:", sum)
