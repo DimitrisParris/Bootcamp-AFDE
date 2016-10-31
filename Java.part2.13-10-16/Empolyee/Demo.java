47a80953305c053f640999520262f7307c3739d8
DimitrisParris <parrisdim@hotmail.com>
2016-10-31 09:57:37 +0200

    Add files via upload

---
+++ Java.part2.13-10-16/Empolyee/Demo.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Demo {
+	public static void main (String []args){
+		Employee emp= new Employee("Helen", "Paulidi", 1991);
+		System.out.println(emp);
+		}
+}
